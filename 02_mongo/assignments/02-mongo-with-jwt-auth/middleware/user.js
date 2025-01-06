6632786dc2fb3cf49f586937f5b73cc8da73fd97
Purushottam Kumar <purushottam1619@gmail.com>
2025-01-07 00:19:29 +0530

    assisgment

--- 02_mongo/assignments/02-mongo-with-jwt-auth/middleware/user.js
+++ 02_mongo/assignments/02-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,39 @@
+const jwt = require("jsonwebtoken");
+const secret = require("../index");
+
 function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+  // Implement user auth logic
+  // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+
+  // get the token from the header
+  const token = req.headers.authorization;
+
+  //Bearer token
+  // convert the token to an array and get the second element
+  const words = token.split(" ");
+  const jwtToken = words[1];
+
+  /*
+    Install the jsonwebtoken package
+    npm install jsonwebtoken
+
+    import the jsonwebtoken 
+    const jwt = require("jsonwebtoken");
+    */
+
+  //verify the token
+  const decodedValue = jwt.verify(jwtToken, secret);
+  if (decodedValue.username) {
+    next();
+  } else {
+    res.status(401).json({ message: "Unauthorized Access" });
+  }
+
+  /*
+  Difference between authentication and authorization
+    Authentication is the process of verifying who you are.
+    Authorization is the process of verifying what you have access to.
+  */
 }
 
 module.exports = userMiddleware;
--- 02_mongo/assignments/02-mongo-with-jwt-auth/routes/admin.js
+++ 02_mongo/assignments/02-mongo-with-jwt-auth/routes/admin.js
@@ -1,22 +1,33 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", async (req, res) => {
+  // Implement admin signup logic
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  await Admin.create({
+    username,
+    password,
+  });
+
+  res.json({ message: "Admin created successfully" });
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
+router.post("/signin", (req, res) => {
+  // Implement admin signup logic
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post("/courses", adminMiddleware, (req, res) => {
+  // Implement course creation logic
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get("/courses", adminMiddleware, (req, res) => {
+  // Implement fetching all courses logic
 });
 
 module.exports = router;
