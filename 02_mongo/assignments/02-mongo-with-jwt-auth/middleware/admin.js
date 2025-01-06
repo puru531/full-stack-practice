de6132b5c72cd344ecb0f7dae56546c4ce8e20a3
Purushottam Kumar <purushottam1619@gmail.com>
2025-01-07 00:21:53 +0530

    assignment

--- 02_mongo/assignments/02-mongo-with-jwt-auth/middleware/admin.js
+++ 02_mongo/assignments/02-mongo-with-jwt-auth/middleware/admin.js
@@ -3,8 +3,6 @@ const secret = require("../index");
 
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
-    // Implement admin auth logic
-    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
   //get the token from the header
   const token = req.headers.authorization; // headers are case-insensitive
 
