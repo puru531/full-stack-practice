9562405346e8cf3231df241ac1d2d6fdf6afe21a
Purushottam Kumar <purushottam1619@gmail.com>
2025-01-17 00:14:33 +0530

    New react project for recoil

---
+++ 03_react/00_recoil/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <button>Home</button>
+
+     <button>My Network()</button>
+     <button>Jobs()</button>
+     <button>Messaging()</button>
+     <button>Notifications()</button>
+
+      <button>Me</button>
+    </>
+  )
+}
+
+export default App
